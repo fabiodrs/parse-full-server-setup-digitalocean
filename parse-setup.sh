659465ea2c629c14a51366ba8f2053a2eafa3145
Fábio Demarchi <fabiodemarchi@gmail.com>
2017-04-19 14:29:46 -0300

    no message

--- parse-setup.sh
+++ parse-setup.sh
@@ -87,7 +87,7 @@ echo -p "Do you have everything you need to start? (y/n)?"
 			sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 			echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.2 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 			sudo apt-get -y update
-			sudo apt-get -y install mongodb-org
+			sudo apt-get -y install mongodb-server
 			service mongod status
 
 			echo " ############### PARSE INSTALL #################"
